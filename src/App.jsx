78fd76079dddd74ad159920893348e59782abc2e
Prachi Tripathi <prachitripathi2727@gmail.com>
2025-08-29 17:06:52 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import Navigation from './components/Navigation';
+const App = () => {
+  return <div>
+    <Navigation/>
+  </div>;
+}
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation = () => {
+    return <div>
+        <nav>
+            <div className="logo">
+                <img src="/images/nike-logo.jpg" width="100" height="50" alt="Nike Logo" />
+            </div>
+            <ul>
+                <li href="#">Home</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    </div>
+}
+export default Navigation;
